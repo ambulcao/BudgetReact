4e277ac4a64a33d38224258a6d5598f711ccb7b8
Alexandre Bulcao <ambulcao@gmail.com>
2020-12-11 22:44:18 +0000

    Creating HomePage Frontend

--- web/pages/index.js
+++ web/pages/index.js
@@ -9,6 +9,13 @@ import {
   NavLink,
   Container,
   Jumbotron,
+  Form,
+  Row,
+  Col,
+  FormGroup,
+  Label,
+  Input,
+  Button,
 } from "reactstrap";
 
 function HomePage() {
@@ -32,14 +39,80 @@ function HomePage() {
         </Container>
       </Navbar>
 
-      <Jumbotron>
+      <Jumbotron className="pg-budget">
+        <style>
+          {`.pg-budget{
+              background-color: #f5fbfa;
+              color: #17a2b8;
+              padding-top: auto;
+              padding-bottom: auto;
+              margin-bottom: 0rem !important;
+          }`}
+        </style>
         <Container>
-          <h1 className="display-3">Hello, world!</h1>
-          <p className="lead">
-            This is a simple hero unit, a simple Jumbotron-style component for
-            calling extra attention to featured content or information.
+          <h1 className="display-5 text-center">
+            Nossos consultores estão prontos para lhe ajudar!
+          </h1>
+          <p className="lead text-center mb-4">
+            Deixe seus contatos abaixo que retornaremos com uma proposta
+            específica para sua necessidade.
           </p>
-          <hr className="my-2" />
+
+          <Form>
+            <Row form>
+              <Col md={6}>
+                <FormGroup>
+                  <Label for="name">Nome</Label>
+                  <Input
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Preencha com seu nome"
+                  />
+                </FormGroup>
+              </Col>
+              <Col md={6}>
+              <FormGroup>
+                  <Label for="email">E-mail</Label>
+                  <Input
+                    type="text"
+                    name="email"
+                    id="email"
+                    placeholder="Informe seu E-mail"
+                  />
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row form>
+              <Col md={6}>
+                <FormGroup>
+                  <Label for="phone">Telefone</Label>
+                  <Input
+                    type="text"
+                    name="phone"
+                    id="phone"
+                    placeholder="Telefone para contato"
+                  />
+                </FormGroup>
+              </Col>
+              <Col md={6}>
+              <FormGroup>
+                  <Label for="whatsApp">WhatsApp</Label>
+                  <Input
+                    type="text"
+                    name="whatsApp"
+                    id="whatsApp"
+                    placeholder="(99) 9999-9999"
+                  />
+                </FormGroup>
+              </Col>
+            </Row>
+            <FormGroup>
+              <Label for="msg">Deixe sua Mensagem</Label>
+              <Input type="textarea" name="msg" id="msg" placeholder="Fale um pouco do seu projeto..."/>
+            </FormGroup>
+            <Button type="submit" outline color="info" >Enviar</Button>
+          </Form>
         </Container>
       </Jumbotron>
     </div>
